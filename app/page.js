e77a243284a9481aa313d841835fe3628291012e
anirudhj2000 <anirudhjoshi985@gmail.com>
2023-09-23 03:46:34 +0530

    minor fix

--- app/page.js
+++ app/page.js
@@ -1,20 +1,18 @@
 'use client'
 import Nav from './components/header/header'
 import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  const [isNodeOpen, setIsNodeOpen] = useState(true);
 
-  const handleNodeOpen = () => {
-    setIsNodeOpen(!isNodeOpen);
-  }
+  const router = useRouter();
+
+  React.useEffect(() => {
+    router.push('/node')
+  }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-end justify-between p-2"> 
-      <button className='justify-end' onClick={handleNodeOpen}>Node</button>
-      <div className={`fixed top-0 right-0 h-screen w-64 bg-gray-200 transition-transform duration-500 transform ${isNodeOpen ? 'translate-x-full' : '-translate-x-0' }`}>
-        <Nav isNodeOpen={isNodeOpen} handleNodeOpen={handleNodeOpen}/>
-      </div>
+    <main className="flex min-h-screen flex-col items-end justify-between p-2">
     </main>
   )
 }
